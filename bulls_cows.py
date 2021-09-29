decf7efc9b935f158c58f919ce9201389c2ee4d3
Olya <koraeva.olya@gmail.com>
2021-09-29 12:39:44 +0300

    Bulls_Cows

---
+++ bulls_cows.py
@@ -0,0 +1,27 @@
+import random
+i = 0
+num = []
+while i < 4:
+    num.append(random.randint(0, 9))
+    i += 1
+print(num)
+user_input = input('Введите число: ')
+user_num = [int(i) for i in str(user_input)]
+def game(num, user_num):
+    cows = 0
+    bulls = 0
+    i = 0
+    while i < 4:
+        if num[i] == user_num[i]:
+                bulls += 1
+        elif not(num[i] == user_num[i]) and num[i] in user_num:
+            cows += 1
+        i += 1
+    print(f'Bulls: {bulls}, \nCows: {cows}')
+    if not (bulls == 4):
+        user_input = input('Введите число: ')
+        user_num = [int(i) for i in str(user_input)]
+        game(num, user_num)
+    else:
+        print('You won!')
+game(num, user_num)
